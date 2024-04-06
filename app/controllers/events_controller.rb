e96e54b6e39f1b4c0db826460077f7fcb08ba190
mutaician <109066669+mutaician@users.noreply.github.com>
2024-04-06 18:15:41 +0300

    events and users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,19 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :location, :date)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "events#index"
+  resources :events, only: [:index, :create]
 end
